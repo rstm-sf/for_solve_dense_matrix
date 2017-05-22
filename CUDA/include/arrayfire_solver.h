c430e3bc8c580cdb067d26df83892ca92470fdad
Rustam Sayfutdinov <rstm.sf@yandex.ru>
2017-05-22 14:18:10 +0500

    В ArrayFire можно так например решать, но медленнее.

--- CUDA/include/arrayfire_solver.h
+++ CUDA/include/arrayfire_solver.h
@@ -11,4 +11,6 @@ int32_t arrayfire_solve(const int32_t n);
 // don't work!
 int32_t arrayfire_solve_test(const int32_t n);
 
+int32_t arrayfire_solve_npi(const int32_t n);
+
 #endif // __ARRAYFIRE_SOLVER_H__
--- CUDA/src/arrayfire_solver.cpp
+++ CUDA/src/arrayfire_solver.cpp
@@ -67,6 +67,74 @@ int32_t arrayfire_solve(const int32_t n) {
 	return 0;
 }
 
+int32_t arrayfire_solve_npi(const int32_t n) {
+	try {
+
+		int32_t device = 0;
+		af::setDevice(device);
+		af::info();
+		cudaStream_t stream = afcu::getStream(device);
+
+		FLOAT *A = (FLOAT *)malloc(n*n * sizeof(FLOAT));
+		FLOAT *X = (FLOAT *)malloc(n * sizeof(FLOAT));
+		fill_matrix(n, n, A, n, 100.0);
+		fill_vector(n, 1, X, n, 10.0);
+		af::array d_A(n, n, A);
+		af::array d_X(n, X);
+		free(A);
+		free(X);
+
+		const af::array B = matmul(d_A, d_X);
+
+		af::array LU = d_A.copy();
+		af::array pivot;
+
+		double t1 = 0.0, t2 = 0.0, t3 = 0.0;
+
+		printf("\nStart af lu...\n");
+		CUDA_TIMER_START(t1, stream);
+		af::luInPlace(pivot, LU, false);
+		CUDA_TIMER_STOP(t1, stream);
+		printf("Stop af lu.\nTime calc: %f (s.)\n", t1);
+		print_to_file_time("af_getrf_time.log", n, t1);
+
+		printf("Start af solveLU...\n");
+		CUDA_TIMER_START(t2, stream);
+		af::array Y = af::solve(lower(LU, true), B(pivot, af::span), AF_MAT_LOWER);
+		d_X = af::solve(upper(LU, false), Y, AF_MAT_UPPER);
+		CUDA_TIMER_STOP(t2, stream);
+		printf("Stop af solveLU.\nTime calc: %f (s.)\n", t2);
+		print_to_file_time("af_getrs_time.log", n, t2);
+
+		printf("Start af matmul...\n");
+		CUDA_TIMER_START(t3, stream);
+		af::array err_sol = matmul(d_A, d_X) - B;
+		CUDA_TIMER_STOP(t3, stream);
+		printf("Stop af matmul.\nTime calc: %f (s.)\n\n", t3);
+		print_to_file_time("af_gemv_time.log", n, t3);
+
+		const double nrm_b = af::norm(B);
+		if (nrm_b <= 1e-24) {
+			printf("norm(b) <= 1e-24!\n");
+			return -1;
+		}
+		
+		const double residual = af::norm(err_sol, AF_NORM_EUCLID);
+		printf("Abs. residual: %e\n", residual);
+		const FLOAT relat_residual = residual / nrm_b;
+		printf("Relative residual: %e\n\n", relat_residual);
+		print_to_file_residual("af_relat_residual.log", n, relat_residual);
+
+	} catch (af::exception& e) {
+
+		fprintf(stderr, "%s\n", e.what());
+		throw;
+
+	}
+
+	return 0;
+}
+
 // don't work!
 int32_t arrayfire_solve_test(const int32_t n) {
 	try {
